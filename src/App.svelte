d8853c4cb1c53843c65c127b21baae8cc1c5bb94
TopGitSan <c.toparcean@gmail.com>
2024-04-23 12:16:13 +0200

    feat(Counter): :sparkles: create Counter component

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
+  import Counter from './lib/Counter.svelte';
 </script>
 
 <main>
   <h1>Vite + Svelte+ RxJs</h1>
 
+  <div class="card">
+    <Counter />
+  </div>
 
 </main>
 
